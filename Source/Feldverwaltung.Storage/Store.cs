5789f1e16348ae49c76b12b6d0dbd2b4d180ef02
Stefan Weber <s.weber@csg-ms.de>
2017-03-24 10:48:14 +0100

    Add storage to project

---
+++ Source/Feldverwaltung.Storage/Store.cs
@@ -0,0 +1,82 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Criterion;
+using NHibernate.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feldverwaltung.Storage
+{
+    public class Store : IDisposable
+    {
+        public ISessionFactory sessionFactory;
+
+        public void Initialize()
+        {
+            var assembly = Assembly.Load("MicrosoftSQLDBPerformanceTest.Mappings");
+
+            sessionFactory = GetSessionFactory(new Assembly[1] { assembly });
+        }
+
+        public virtual StoreSession GetStoreSession()
+        {
+            StoreSession storeSession = new StoreSession();
+            storeSession.session = sessionFactory.OpenSession();
+            storeSession.session.FlushMode = FlushMode.Commit;
+            return storeSession;
+        }
+
+        private ISessionFactory GetSessionFactory(IList<Assembly> mappingAssemblys)
+        {
+            IPersistenceConfigurer config = null;
+            config = MsSqlConfiguration.MsSql2005.ConnectionString(x => x.TrustedConnection()
+                .Server(@"WS-415\SPARK")
+                .Database(@"spArkDB")
+                .TrustedConnection())
+                .AdoNetBatchSize(0)
+                .FormatSql()
+                .DefaultSchema("dbo");
+            Action<MappingConfiguration> orderFluentMapping = createFluentMapping(mappingAssemblys);
+            try
+            {
+                Configuration NHConfig =
+                    Fluently.Configure().Database(config).Mappings(orderFluentMapping).BuildConfiguration();
+                return NHConfig.BuildSessionFactory();
+            }
+            catch (Exception ex)
+            {
+                if (ex.InnerException != null)
+                {
+
+                    Console.WriteLine("Error on creating session factory Details: {0}", ex.InnerException.Message);
+                }
+                Console.WriteLine(string.Format("Error on creating session factory Details: {0}", ex.Message));
+                throw;
+            }
+        }
+
+        private Action<MappingConfiguration> createFluentMapping(IList<Assembly> mappingAssemblys)
+        {
+            Action<MappingConfiguration> orderFluentMapping = (MappingConfiguration MC) =>
+            {
+                FluentMappingsContainer FMC = MC.FluentMappings;
+                mappingAssemblys.ToList().ForEach(Assembly => FMC.AddFromAssembly(Assembly));
+                FMC.Conventions.Add(FluentNHibernate.Conventions.Helpers.DefaultLazy.Never());
+            };
+
+            return orderFluentMapping;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Source/Feldverwaltung.Storage/StoreSession.cs
@@ -0,0 +1,107 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Criterion;
+using NHibernate.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feldverwaltung.Storage
+{
+    public class StoreSession
+    {
+        public ISession session { get; set; }
+        private ITransaction transaction;
+
+
+        public virtual TResult LoadById<TResult>(Guid id)
+        {
+            try
+            {
+                return session.Get<TResult>(id);
+            }
+            catch (ObjectNotFoundException)
+            {
+
+            }
+            return default(TResult);
+        }
+
+        public virtual IList<TResult> LoadAll<TResult>()
+        {
+            return session.Query<TResult>().ToList();
+        }
+
+        public virtual IList<TResult> LoadByPropertyWithValue<TResult>(
+            string propertyName, object propertyValue) where TResult : class
+        {
+            var criteriaQuery = session.CreateCriteria<TResult>();
+            criteriaQuery.Add(Restrictions.Eq(propertyName, propertyValue));
+            return criteriaQuery.List<TResult>();
+        }
+
+        public virtual IList<TResult> LoadByPropertiesWithValues<TResult>(
+            Expression<Func<TResult, bool>> condition) where TResult : class
+        {
+            return session.Query<TResult>().Where(condition).ToList<TResult>();
+        }
+
+        public void Close()
+        {
+            session.Close();
+        }
+
+        public virtual void BeginTransaction()
+        {
+            transaction = session.BeginTransaction(System.Data.IsolationLevel.ReadUncommitted);
+        }
+
+        public virtual void CommitTransaction()
+        {
+            transaction.Commit();
+        }
+
+        public virtual void RollbackTransaction()
+        {
+            transaction.Rollback();
+        }
+
+        public virtual void Save(object entity)
+        {
+            session.Save(entity);
+        }
+
+        public virtual void Delete(object entity)
+        {
+            session.Delete(entity);
+        }
+
+        public void Dispose()
+        {
+            if (transaction != null)
+            {
+                if (transaction.IsActive)
+                {
+                    RollbackTransaction();
+                }
+                transaction.Dispose();
+                transaction = null;
+            }
+            if (session != null)
+            {
+                if (session.IsOpen)
+                {
+                    Close();
+                }
+                session.Dispose();
+                session = null;
+            }
+        }
+    }
+}
